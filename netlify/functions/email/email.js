d6b2d2e363e538854045aaeb2de1413b905ab4fc
Derek Rushforth <derekrushforth@users.noreply.github.com>
2023-04-18 16:45:59 -0700

    Digest email template

--- netlify/functions/email/email.js
+++ netlify/functions/email/email.js
@@ -1,3 +1,6 @@
+const lastFMToken = process.env.LAST_FM_API_TOKEN;
+const lastFMUser = 'bordesak';
+
 const fetch = require('node-fetch');
 
 const handler = async function (request, context) {
@@ -12,6 +15,25 @@ const handler = async function (request, context) {
     return
   }
   console.log(body)
+
+  const tracks = await getTracks();
+  let templateData = {
+    tracks: []
+  }
+
+  tracks.recenttracks.track.forEach((item, index) => {
+    if (index > 11) return
+
+    const track = {
+      artist: item.artist['#text'],
+      album: item.album['#text'],
+      image: item.image[2]['#text'],
+      name: item.name,
+      url: item.url
+    }
+    templateData.tracks.push(track)
+  })
+
   const results = await fetch(
     `${process.env.URL}/.netlify/functions/emails/digest`,
     {
@@ -22,10 +44,8 @@ const handler = async function (request, context) {
       body: JSON.stringify({
         from: process.env.POSTMARK_FROM_ADDRESS,
         to: body.email,
-        subject: `test`,
-        parameters: {
-          
-        },
+        subject: 'Rian’s recent tracks',
+        parameters: templateData,
       }),
     }
   );
@@ -45,3 +65,19 @@ const handler = async function (request, context) {
 
 module.exports = { handler }
 
+async function getTracks() {
+  const url = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${lastFMUser}&api_key=${lastFMToken}&format=json`
+  const results = await fetch(url);
+
+  // If there was an error
+  if (!results.ok) {
+    console.error(results);
+    return
+  }
+
+  // Get JSON body from results
+  const data = await results.json();
+
+  // Return data
+  return data
+}
